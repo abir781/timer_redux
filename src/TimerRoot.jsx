de60edc035647213e3aedae1f406b07bbb6081eb
abir781 <hasan.abir3176@gmail.com>
2026-02-15 09:39:06 +0600

    it's working

--- src/TimerRoot.jsx
+++ src/TimerRoot.jsx
@@ -16,20 +16,32 @@ const TimerRoot = () => {
    
   };
 
+  const handleplay = () =>{
+    console.log("jjjkk");
+
+    const hours = parseInt(time.slice(0,2));
+    const minutes = parseInt(time.slice(2,4));
+    const seconds = parseInt(time.slice(4,6));
+
+    console.log(minutes);
+    console.log(seconds);
+    console.log(hours);
+  }
+
   // Format HH:MM:SS
-//   const formattedTime = `${time.slice(0,2)}:${time.slice(2,4)}:${time.slice(4,6)}`;
+  const formattedTime = `${time.slice(0,2)}:${time.slice(2,4)}:${time.slice(4,6)}`;
 
   return (
     <div className="flex items-center justify-center min-h-screen gap-4">
       <div className="flex gap-5">
         <input
           type="text"
-          value={time}
+          value={formattedTime}
           onChange={handleChange}
           className="rounded focus:outline-none text-[#edfa51] text-[44px] font-bold w-[400px] h-[80px] bg-[#77c64c] text-center"
         />
 
-        <button className="bg-green-500 text-white px-12 h-[80%] mt-[8px] rounded-xl">
+        <button onClick={handleplay} className="bg-green-500 text-white px-12 h-[80%] mt-[8px] rounded-xl">
           <FaPlay size={25} className="text-[#edfa51]" />
         </button>
       </div>
